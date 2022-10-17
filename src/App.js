62f84b564f01a7107538427345c238e6ccc3f77c
Theophilus Muganhu <theotakumug@gmail.com>
2022-10-17 22:25:07 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [minutes, setMinutes] = useState(1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-center">
+      <section>
+        <div>
+          <label htmlFor='minutes'>Count down minutes: </label>
+          <input 
+            id='minutes' 
+            type='number' 
+            placeholder='(min)' 
+            value={minutes}
+            onChange={(e) => setMinutes(e.target.value)}
+            min={1}
+            >
+            </input>
+        </div>
+        <div>
+          <button>START</button>
+          <button>PAUSE</button>
+        </div>
+        <div>
+          <p>You are half way complete!</p>
+        </div>
+        <div>
+          <p>Alomst done</p>
+        </div>
+        <div>
+          <p>00:00</p>
+        </div>
+        <div>
+          <button>x1</button>
+          <button>x1.5</button>
+          <button>x2</button>
+        </div>
+        
+      </section>
     </div>
   );
 }
